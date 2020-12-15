4116da6dfac3da1e8f098b34a05dd8971f936c2c
Chang Lyu <chang_lyu@outlook.com>
2020-12-14 22:51:57 -0500

    create a movie search engine

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,14 @@
 import './App.css';
+import MovieSearchApp from './MovieSearchApp';
 
+// TODO: use router to config different app
 function App() {
   return (
     <div>
       <h1 class="App">
         Hello Chang
     </h1>
-      <h2>Menu</h2>
-      <ul>
-        <li>Add Image</li>
-        <li>Cloth collection</li>
-      </ul>
+      <MovieSearchApp></MovieSearchApp>
     </div>
   );
 }
