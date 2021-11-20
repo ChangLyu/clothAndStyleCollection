d1b1f3c589c9a5aa461f6aa548b6b7fa2ff7833c
ChangLyu <Chang_Lyu@outlook.com>
2021-11-19 22:08:15 -0500

    update

--- src/components/App.js
+++ src/components/App.js
@@ -12,6 +12,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'antd/dist/antd.css';
 import { VideoCameraOutlined, EditOutlined, BookOutlined } from '@ant-design/icons';
 import AlgorithmApp from './algorithm/AlgorithmApp';
+import { Container } from 'react-bootstrap';
 /**TODO:
  * 3. refactor movie search app, refactor reducer out
  * 3. add more functions, ranks system
@@ -20,40 +21,42 @@ import AlgorithmApp from './algorithm/AlgorithmApp';
 function App() {
   return (
     <div>
-      <h1>Chang's React Practise Website</h1>
-      <Router>
-        <div>
-          <div className="menu-container">
-            <Menu mode="horizontal">
-              <Menu.Item icon={<VideoCameraOutlined />} key="moviesearch">
-                <Link to="/moviesearch">Search Movie</Link>
-              </Menu.Item>
-              <Menu.Item icon={<EditOutlined />} key="todoappRedux">
-                <Link to="/todoapp">To do App with local Redux</Link>
-              </Menu.Item>
-              <Menu.Item icon={<BookOutlined />} key="algorithm">
-                <Link to="/algorithm">Algorithm</Link>
-              </Menu.Item>
-            </Menu>
-          </div>
+      <Container>
+        <h1>Chang's React Practise App</h1>
+        <Router>
+          <div>
+            <div className="menu-container">
+              <Menu mode="horizontal">
+                <Menu.Item icon={<VideoCameraOutlined />} key="moviesearch">
+                  <Link to="/moviesearch">Search Movie</Link>
+                </Menu.Item>
+                <Menu.Item icon={<EditOutlined />} key="todoappRedux">
+                  <Link to="/todoapp">To do App with local Redux</Link>
+                </Menu.Item>
+                <Menu.Item icon={<BookOutlined />} key="algorithm">
+                  <Link to="/algorithm">Algorithm</Link>
+                </Menu.Item>
+              </Menu>
+            </div>
 
-          {/* A <Switch> looks through its children <Route>s and
+            {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-          <div className="main-content">
-            <Switch>
-              <Route path="/moviesearch">
-                <MovieSearchApp />
-              </Route>
-              <Route path="/todoapp">
-                <TodoApp />
-              </Route>
-              <Route path="/algorithm">
-                <AlgorithmApp />
-              </Route>
-            </Switch>
+            <div className="main-content">
+              <Switch>
+                <Route path="/moviesearch">
+                  <MovieSearchApp />
+                </Route>
+                <Route path="/todoapp">
+                  <TodoApp />
+                </Route>
+                <Route path="/algorithm">
+                  <AlgorithmApp />
+                </Route>
+              </Switch>
+            </div>
           </div>
-        </div>
-      </Router>
+        </Router>
+      </Container>
     </div>
   );
 }
--- src/components/ClothCollection.js
+++
@@ -1,3 +0,0 @@
-function ClothCollection(props) {
-    return <h1>Hello, {props.name}</h1>;
-}
